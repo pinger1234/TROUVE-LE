c0c51444df6cb7978e1bc1bfc43517eccef6c1a9
Anicet Cyrille KAMBOU <kanicetcyrille@gmail.com>
2023-04-06 07:55:26 +0000

    Sign-up geré

---
+++ _database.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+# 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+# [0] [Importation des modules]
+# 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+from __future__ import unicode_literals
+from deta import *
+import bcrypt
+from flask import request, jsonify
+
+# 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+# [1] [Creation des fonctions pour l'ajout d'utilisateur ]
+# 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+
+# Token = "GvU7GWAn_4ZAs2z8fPiCk6HzxHajpRjDq4qAuYM45"
+# Datakey = "a03qi4zrdq7_Tr6fSQyVRzP9u7xgbyssQSoZzEeeceQY"
+# deta = Deta(Datakey)
+
+salt = bcrypt.gensalt()
+
+
+def add_user(db, user):
+    bytes = user.password.encode("utf-8")
+    hashed = bcrypt.hashpw(bytes, salt)
+    print("===========================")
+    print(hashed)
+    try:
+        db.put(
+            {
+                "key": user.tel,
+                "pp": user.pp,
+                "nom": user.nom,
+                "prenoms": user.prenoms,
+                "naissance": user.naissance,
+                "piece": user.piece,
+                "npiece": user.npiece,
+                "tel": user.tel,
+                "email": user.email,
+                "password": hashed.decode("utf-8"),
+            }
+        )
+        print("-------User enregistré")
+        return True
+    except:
+        print("++++ user pas enrégistré")
+        return False
+
+
+def save_file(drive_name, file_name, file_data):
+    try:
+        drive_name.put(file_name, file_data)
+        print("-------Image user sauvegardé")
+        return True
+    except:
+        print("++++ erreur sauvegarde image")
+        return False
+
+
+def get_user(db, key):
+    user = db.get(key)
+    return user if user else False
+
+
+def user_exist(db, key):
+    user = db.get(key)
+    return True if user else False
+
+
+def get_all(db):
+    print("-----telechargement de base de donnée")
+    return db.fetch().items
+
+
+def update_user(db, key, new_data):
+    user = db.put(new_data, key)
+    return user
+
+
+def delete_user(db, key):
+    db.delete(key)
+    return jsonify({"status": "ok"}, 200)
+
+
+def pass_correct(passeword, hashed):
+    userBytes = passeword.encode("utf-8")
+    result = bcrypt.checkpw(userBytes, hashed)
+    return result
+
+
+def authentification(key, passeword):
+    if get_user(key):
+        if pass_correct(passeword, get_user(key).password):
+            return True
+        else:
+            return False
+    else:
+        return None
+
+
+# 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+# [3] [Creation des objets pour la base de données ]
+# 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
+
+
+class utilisateur:
+    def __init__(self):
+        self.key = ""
+        self.pp = ""
+        self.nom = ""
+        self.prenoms = ""
+        self.naissance = ""
+        self.piece = ""
+        self.npiece = ""
+        self.tel = ""
+        self.email = ""
+        self.passeword = ""
--- trouvele.py
+++ trouvele.py
@@ -13,10 +13,13 @@ from flask import (
     redirect,
     url_for,
 )  # pip install flask
-from pydantic import BaseModel  # pip install pydantic
+
+# from pydantic import BaseModel  # pip install pydantic
+# from cookies import *  # pip install cookies
+# from PIL import Image # pip install pillow
 import os
-from cookies import *  # pip install cookies
-from PIL import Image
+from _database import *
+from _forms import *
 
 # --------------------------- {Module de chiffrage} ----------------------------------------
 from hashlib import *
@@ -29,9 +32,11 @@ import re
 # 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
 # ================ {initialisation du projet} ===============
 
-Token = "GvU7GWAn_4ZAs2z8fPiCk6HzxHajpRjDq4qAuYM45"
-Datakey = "a03qi4zrdq7_Tr6fSQyVRzP9u7xgbyssQSoZzEeeceQY"
-deta = Deta(Datakey)
+# Token = "GvU7GWAn_4ZAs2z8fPiCk6HzxHajpRjDq4qAuYM45"
+# Datakey = "a03qi4zrdq7_Tr6fSQyVRzP9u7xgbyssQSoZzEeeceQY"
+
+cle_trouve_le = "a0azww1wj87_tJRPYB72Z9XWSpd5Nx4n6VMTNaUfakcZ"
+deta = Deta(cle_trouve_le)
 
 
 # ================ {Base de données} ===============
@@ -40,15 +45,16 @@ users = deta.Base("users")
 declaration = deta.Base("declaration")
 type_objet = deta.Base("type_objet")
 
+# ================ {drive} ===============
+users_pp = deta.Drive("users_pp")
+identite = deta.Drive("identite")
+img_declaration = deta.Drive("img_declaration")
+
 # ================ {Variables} ==================
 
 # photo_profil = Image.open("static/img/logo.png",mode="r")
 # photo_piece = Image.open("static/img/logo.png",mode="r")
 
-# ================ {drive} ===============
-pp = deta.Drive("pp")
-identite = deta.Drive("identite")
-img_declaration = deta.Drive("img_declaration")
 
 # 〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜
 # [2] [definition des fonctions ]
@@ -70,67 +76,118 @@ def carousel():
 
 @app.route("/login/", methods=["POST", "GET"])
 def login():
+    print(get_all(users))
     return render_template("login.html")
 
 
 @app.route("/signup/", methods=["POST", "GET"])
 def signup():
-    global photo_piece, photo_profil
+    # global photo_piece, photo_profil
     # print ("la methode de requet est :", request.method)
+    print(get_all(users))
     if request.method == "POST":
-        name = request.form["first_name"]
-        second_name = request.form["secondname"]
-        birthday = request.form["birthday"]
-        number_piece = request.form["number_piece"]
-        type_piece = request.form["type_piece"]
-        contact = request.form["contact"]
-        email = request.form["email"]
-        password = request.form["password"]
+        user = utilisateur()
+        user.nom = request.form["nom"]
+        user.prenoms = request.form["prenoms"]
+        user.naissance = request.form["naissance"]
+        user.npiece = request.form["npiece"]
+        user.piece = request.form["piece"]
+        user.tel = request.form["tel"]
+        user.key = user.tel
+        user.email = request.form["email"]
+        user.password = request.form["password"]
         passwordconfirm = request.form["passwordconfirm"]
+        user.pp = str(user.tel) + ".webp"
 
-        # Creation d'image par defaut de photo de profile et de piece d'identité
-        # photo_profil = Image.open(url_for("static",filename="img/logo.png"), mode="rb")
-        # photo_piece = Image.open(url_for("static",filename="img/logo.png"), mode="rb")
+        if user_exist(users, user.tel):
+            return render_template(
+                "signup.html",
+                message="user-exist",
+            )
+        elif (
+            user_exist(users, user.tel) == False
+        ) and passwordconfirm != user.password:
+            return render_template(
+                "signup.html",
+                message="passe-confirm-error",
+            )
+        elif (
+            (user_exist(users, user.tel) == False)
+            and passwordconfirm == user.password
+            and passwordconfirm != ""
+        ):
+            if request.files["pp"]:
+                file_data = request.files.get("pp")
+                file_ext = file_data.filename.split(".")[-1]
+                user.pp = str(user.tel) + "." + file_ext
+                # print("Donnees image:-----------------")
+                # print(file_data)
+                # print("Nom fichier image:-----------------")
+                # print(file_name)
+                save_file(users_pp, user.pp, file_data)
+                result = add_user(users, user)
+            print("reponse d'ajout utilisateur:", result)
+            else:
+                file_data = open("static/img/profile-default.png", "rb")
+                print(file_data.readlines())
+                save_file(users_pp, str(user.tel) + ".png", file_data.readlines())
+                file_data.close()
+            if result == False:
+                return render_template(
+                    "signup.html",
+                    message="erreur",
+                )
+            else:
+                return redirect(url_for("login"))
+        else:
+            return render_template(
+                "signup.html",
+                message="passe-vide",
+            )
+    else:
+        return render_template("signup.html", message="")
+    # Creation d'image par defaut de photo de profile et de piece d'identité
+    # photo_profil = Image.open(url_for("static",filename="img/logo.png"), mode="rb")
+    # photo_piece = Image.open(url_for("static",filename="img/logo.png"), mode="rb")
 
-        # photo_profil = Image.open("static/img/logo.png", mode="r")
-        photo_profil = iio.imread("imageio:static/img/logo.png")
-        # photo_piece = Image.open("static/img/logo.png", mode="r")
-        photo_piece = iio.imread("imageio:static/img/logo.png")
+    # photo_profil = Image.open("static/img/logo.png", mode="r")
+    # photo_profil = iio.imread("imageio:static/img/logo.png")
+    # # photo_piece = Image.open("static/img/logo.png", mode="r")
+    # photo_piece = iio.imread("imageio:static/img/logo.png")
 
-        file_ext = "png"
+    # file_ext = "png"
 
-        # recupération des images depuis le formulaire si elle ont été envoyées
-        # if request.files["photo_profil"]:
-        #     photo_profil = request.files.get("photo_profil")
-        #     file_ext=photo_profil.filename.split(".")[-1]
+    # recupération des images depuis le formulaire si elle ont été envoyées
+    # if request.files["photo_profil"]:
+    #     photo_profil = request.files.get("photo_profil")
+    #     file_ext = photo_profil.filename.split(".")[-1]
 
-        # if request.files["photo_piece"]:
-        #     photo_piece = request.files.get("photo_piece")
-        #     file_ext=photo_piece.filename.split(".")[-1]
+    # if request.files["photo_piece"]:
+    #     photo_piece = request.files.get("photo_piece")
+    #     file_ext=photo_piece.filename.split(".")[-1]
 
-        # Ajout des images dans le drive de deta
-        pp.put(number_piece + "." + file_ext, photo_piece)
-        identite.put(number_piece + "." + file_ext, photo_piece)
+    # Ajout des images dans le drive de deta
+    # pp.put(number_piece + "." + file_ext, photo_piece)
+    # identite.put(number_piece + "." + file_ext, photo_piece)
 
-        # Sauvegarde des données dans la base de données users
-        users.put(
-            {
-                "First_name": name,
-                "Last_name": second_name,
-                "Birthday": birthday,
-                "Number_of_piece": number_piece,
-                "Type_piece": type_piece,
-                "Photo_piece": "identite/" + number_piece + "." + file_ext,
-                "Photo_profil": "pp/" + number_piece + "." + file_ext,
-                "Contact": contact,
-                "Email": email,
-                "Password": password,
-                "Password_confirm": passwordconfirm,
-            }
-        )
+    # # Sauvegarde des données dans la base de données users
+    # users.put(
+    #     {
+    #         "First_name": name,
+    #         "Last_name": second_name,
+    #         "Birthday": birthday,
+    #         "Number_of_piece": number_piece,
+    #         "Type_piece": type_piece,
+    #         "Photo_piece": "identite/" + number_piece + "." + file_ext,
+    #         "Photo_profil": "pp/" + number_piece + "." + file_ext,
+    #         "Contact": contact,
+    #         "Email": email,
+    #         "Password": password,
+    #         "Password_confirm": passwordconfirm,
+    #     }
+    # )
 
-        return render_template("login.html")
-    return render_template("signup.html")
+    # return render_template("login.html")
 
 
 if __name__ == "__main__":
